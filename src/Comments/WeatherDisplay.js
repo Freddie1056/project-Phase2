f667bc79aefa64dab8206a030ae826a1755cab36
Fredrick <freddyfred976@gmail.com>
2023-08-03 18:36:55 +0300

    finally2

--- src/Comments/WeatherDisplay.js
+++ src/Comments/WeatherDisplay.js
@@ -22,38 +22,3 @@ function WeatherDisplay({ data }) {
 
 export default WeatherDisplay;
 
-
-
-// import React from 'react';
-
-// function WeatherDisplay({ data }) {
-//   return (
-//     <div className='container'>
-//       {data.main && (
-//         <div className='top bg-blue-200 p-4 rounded-lg'>
-//           <div className='location'>
-//             <p className='text-xl font-bold'>{data.name}</p>
-//           </div>
-//           <div className='temp'>
-//             <h1 className='text-4xl font-semibold'>{data.main.temp}°F</h1>
-//           </div>
-//           <div className='description'>
-//             <p className='text-lg'>{data.weather[0].main}</p>
-//           </div>
-//         </div>
-//       )}
-//       {data.main && (
-//         <div className='bottom mt-4 bg-blue-100 p-4 rounded-lg'>
-//           <div className='humidity'>
-//             <p>Humidity: {data.main.humidity}%</p>
-//           </div>
-//           <div className='wind'>
-//             <p>Wind Speed: {data.wind.speed}MPH</p>
-//           </div>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default WeatherDisplay;
