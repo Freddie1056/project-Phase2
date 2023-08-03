5d9f6e059ff9ad76faa7c5f56986768d7eadf298
Fredrick <freddyfred976@gmail.com>
2023-08-03 17:46:23 +0300

    finally

--- src/Comments/About.js
+++ src/Comments/About.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='flex justify-center items-center h-screen bg-blue-100'>
+      <p className='font-semibold mb-4 text-center'>
+      Experience Simplicity: Our weather app brings convenience to the forefront. Just enter the name of any city, and effortlessly receive accurate weather results. No hassle, no complexity—just quick and reliable forecasts at your fingertips.
+      </p>
+    </div>
   )
 }
 
-export default About
+export default About;
+
+
+
--- src/Comments/Home.js
+++ src/Comments/Home.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-        <p></p>
+    <div className='flex justify-center items-center h-screen bg-gray-200'>
+      <div>
+      <h1 class="text-4xl font-bold mb-4 text-center">Welcome to Our Weather App</h1>
+        <p class="text-gray-700 text-lg mb-6 text-center font-light">
+            With our app, you'll be able to accurately predict the weather for any city you desire.
+        </p>
+      </div>
     </div>
   )
 }
 
 export default Home
+
+
