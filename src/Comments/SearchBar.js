a2cd4b0711da76d9fde74831cdae3bbcd9c55315
Fredrick <freddyfred976@gmail.com>
2023-08-03 18:39:27 +0300

    finally3

--- src/Comments/SearchBar.js
+++ src/Comments/SearchBar.js
@@ -17,24 +17,3 @@ function SearchBar({ location, setLocation, searchLocation }) {
 }
 
 export default SearchBar;
-
-
-
-// import React from 'react';
-
-// function SearchBar({ location, setLocation, searchLocation }) {
-//   return (
-//     <div className='search'>
-//       <input
-//         value={location}
-//         onChange={(event) => setLocation(event.target.value)}
-//         onKeyPress={searchLocation}
-//         placeholder='Enter City'
-//         type='text'
-//         className='border rounded-lg px-4 py-2'
-//       />
-//     </div>
-//   );
-// }
-
-// export default SearchBar;
